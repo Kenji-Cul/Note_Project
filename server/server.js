a794e44c0ad4986d2f91e9429e760a9867641228
Gideon Teibo <92151531+Kenji-Cul@users.noreply.github.com>
2024-03-30 11:19:27 +0000

    git commit -a

--- server/server.js
+++ server/server.js
@@ -6,18 +6,31 @@ require("dotenv").config();
 
 // Import dependencies
 const express = require('express');
+const cors = require("cors");
 const connectToDb = require('./config/connectToDb');
+const notesController = require('./controllers/notesController');
 
 // Create an express app
 const app = express();
+
+// Configure express app
+app.use(express.json());
+app.use(cors());
 
 // Connect to database
 connectToDb();
 
 // Routing
-app.get('/', (req, res) => {
-    res.json({ hello : "world"});
-});
+
+app.get('/notes', notesController.fetchNotes);
+
+app.get('/notes/:id', notesController.fetchNote)
+
+app.post('/notes', notesController.createNote);
+
+app.put('/notes/:id', notesController.updateNote);
+
+app.delete('/notes/:id', notesController.deleteNote);
 
 // Start our server
 app.listen(process.env.PORT);
