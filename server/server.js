59619ac7c523a9da5e24042e90d6ee5c0e2053c6
Gideon Teibo <92151531+Kenji-Cul@users.noreply.github.com>
2024-04-01 17:15:10 +0100

    git commit -a

--- server/server.js
+++ server/server.js
@@ -7,20 +7,34 @@ require("dotenv").config();
 // Import dependencies
 const express = require('express');
 const cors = require("cors");
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./config/connectToDb');
 const notesController = require('./controllers/notesController');
+const usersController = require('./controllers/usersController');
+const  requireAuth = require('./middleware/requireAuth');
 
 // Create an express app
 const app = express();
 
 // Configure express app
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({
+    origin: true,
+    credentials: true,
+}
+));
 
 // Connect to database
 connectToDb();
 
 // Routing
+app.post('/signup', usersController.signup);
+
+app.post('/login', usersController.login);
+
+app.get('/logout', usersController.logout);
+app.get('/check-auth', requireAuth, usersController.checkAuth);
  
 app.get('/notes', notesController.fetchNotes);
 
